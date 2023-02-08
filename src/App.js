b110b5cd262815b018183d79d668377471da855a
Mandy Meindersma <meinders@ualberta.ca>
2023-02-08 04:03:58 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          GitHub Codespaces  and Mandy <span className="heart">♥️</span> React
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+        <label for="vehicle1"> I have a bike</label></br>
+        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+        <label for="vehicle2"> I have a car</label></br>
+        <button>Click me!</button>
+
+        <br />
+        
         <p>
           <a
             className="App-link"
